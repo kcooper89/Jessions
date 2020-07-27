7e0232eaebbf8facf8d158c3bb63cce979d1ac8b
Kyle Cooper <kcooper89.kc@gmail.com>
2020-07-26 23:32:16 -0400

    Updates

---
+++ precache-manifest.c86ba7cb227ae6d309793bd2443dcec5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93a2f2e9a435c8f35ca7e847e1c405d1",
+    "url": "/Jessions/index.html"
+  },
+  {
+    "revision": "c646d066982b6b3081e6",
+    "url": "/Jessions/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "32db61e769bb53afd145",
+    "url": "/Jessions/static/css/main.38b6a2c3.chunk.css"
+  },
+  {
+    "revision": "c646d066982b6b3081e6",
+    "url": "/Jessions/static/js/2.7a27bc3f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Jessions/static/js/2.7a27bc3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32db61e769bb53afd145",
+    "url": "/Jessions/static/js/main.abca87b6.chunk.js"
+  },
+  {
+    "revision": "0fa13cc3f2db587710cb",
+    "url": "/Jessions/static/js/runtime-main.03cb4541.js"
+  }
+]);
